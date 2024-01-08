20b37101853077848c3c5720865551782d7ffa65
Agustin Castiarena <castiarena@gmail.com>
2024-01-08 17:50:38 -0300

    adjust and logo

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,15 @@
-import { ChakraBaseProvider, VStack } from '@chakra-ui/react'
+import { ChakraBaseProvider, Image, VStack } from '@chakra-ui/react'
 import { theme } from '../theme'
 import { Timer } from '../Timer'
 import dayjs from 'dayjs'
+import rawr from '../../public/rawr-ico.svg'
 
 export const App = () => {
   return (
     <ChakraBaseProvider theme={theme}>
+      <VStack py={4}>
+        <Image src={rawr} />
+      </VStack>
       <VStack>
         <Timer
           to={dayjs('01/27/2024').toDate()}
